7777b3f2745eb077adef342abe19386c5f37e721
chloejiwon <chloe326o.o@gmail.com>
2017-11-14 23:54:30 +0900

    Player COntroller 수정

--- FloatingSpace/Assets/Source/PlayerController.cs
+++ FloatingSpace/Assets/Source/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +6,25 @@ public class PlayerController: MonoBehaviour {
 
 	public float rotateSpeed = 45f;
 
+	// 너무 오래 스페이스바 눌렀을 경우에, Player 지침!
+	bool IsTired;
+	float Timer;
+	float waitTime=2f;
+
 	public bool canMove=true;
 	public cnr NPC;
 
 	// Use this for initialization
 	void Start () {
 		NPC = FindObjectOfType<cnr> ();
-
+		IsTired = false;
+		Timer = 0f;
 	}
 	void OnTriggerEnter(Collider col){
 		if (col.tag == "character") {
 			// conversation should be starting
 			//print(col.gameObject.name);
-		
+
 		}
 	}
 	// Update is called once per frame
@@ -27,20 +33,28 @@ public class PlayerController: MonoBehaviour {
 		if (!canMove) {
 			return;
 		}
-		if (Input.GetKey (KeyCode.Space)) {
+		if(!IsTired){
+				if (Input.GetKey (KeyCode.Space)) {
+				/*	if (NPC.name == "Axis")
+						NPC.SpeedUp ();
+		*/
+					transform.Translate(Vector3.forward * Time.deltaTime);
+					rotateSpeed += 50* Time.deltaTime;
+					if (rotateSpeed >= 300) {
+						// Stop Spinning !!
+						// Character tired
+						IsTired = true;
+						rotateSpeed = 1;
 
-			if (NPC.name == "Axis")
-				NPC.SpeedUp ();
-			
-			rotateSpeed += 50* Time.deltaTime;
-			if (rotateSpeed >= 300) {
-				// Stop Spinning !! 
-				// Character tired
-				rotateSpeed = 1; 
-
-			}
+					}
+				}
+		}else{
+			//지쳤을 경우
+				if(Timer >= waitTime){
+					//2초 지났을 경우에
+					IsTired = true;
+				}
 		}
-
 		transform.Rotate (Vector3.back * rotateSpeed * Time.deltaTime);
 
 	}
