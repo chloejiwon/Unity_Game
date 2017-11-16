800c23d493be6b10bbefc3409c196d4d875ad53c
chloejiwon <chloe326o.o@gmail.com>
2017-11-16 13:33:38 +0900

    PlayerController + Timer

--- FloatingSpace/Assets/Source/PlayerController.cs
+++ FloatingSpace/Assets/Source/PlayerController.cs
@@ -1,66 +1,81 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController: MonoBehaviour {
 
 	public float rotateSpeed = 45f;
+	public float forwardSpeed = 0f;
 
-	// 너무 오래 스페이스바 눌렀을 경우에, Player 지침!
+	// Too many speed.. character needs to take a rest a little while!!!
 	bool IsTired;
-	float Timer;
-	float waitTime=2f;
+	float timer;
+	float fps;
+	float waitingTime = 5;
 
 	public bool canMove=true;
 	public cnr NPC;
 
+    public string[] Members;
+
+    public int score;
+
 	// Use this for initialization
 	void Start () {
 		NPC = FindObjectOfType<cnr> ();
-		IsTired = false;
-		Timer = 0f;
+        score = 0;
+		timer = 0.0f;
 	}
+
 	void OnTriggerEnter(Collider col){
+		IsTired = false;
+
 		if (col.tag == "character") {
 			// conversation should be starting
 			//print(col.gameObject.name);
-
 		}
 	}
 	// Update is called once per frame
 	void Update () {
-
+		
 		if (!canMove) {
 			return;
 		}
-		if(!IsTired){
-				if (Input.GetKey (KeyCode.Space)) {
-				/*	if (NPC.name == "Axis")
-						NPC.SpeedUp ();
-		*/
-					transform.Translate(Vector3.forward * Time.deltaTime);
-					rotateSpeed += 50* Time.deltaTime;
-					if (rotateSpeed >= 300) {
-						// Stop Spinning !!
-						// Character tired
-						IsTired = true;
-						rotateSpeed = 45f;
 
-					}
+		if (!IsTired){
+			if (Input.GetKey (KeyCode.Space)) {
+
+				if (rotateSpeed <= 300) {
+					transform.Translate (Vector3.forward * Time.deltaTime);
 				}
-		}else{
-			//지쳤을 경우 waitTime만큼 기다리고
-			// 다시 Player Controll할 수 있게 
-			Timer += Time.deltaTime;
-				if(Timer >= waitTime){
-					//2초 지났을 경우에
+
+				//		if (NPC.name == "Axis")
+				//			NPC.SpeedUp ();
+			
+				rotateSpeed += 50 * Time.deltaTime;
+				//		forwardSpeed += (0.1f) * Time.deltaTime;
+				if (rotateSpeed >= 300) {
+					// Stop Spinning !! 
+					// Character tired
+					rotateSpeed = 1; 
+					forwardSpeed = 0;
 					IsTired = true;
-					Timer = 0f;
+
 				}
+			}
+				
+		} else {
+			// If Tired..
+			timer+= Time.deltaTime;
+			print ("TIRED");
+			if (timer > waitingTime) {
+				timer = 0;
+				IsTired = false;
+			}
 		}
 		transform.Rotate (Vector3.back * rotateSpeed * Time.deltaTime);
 
 	}
-
+    
 
 }
