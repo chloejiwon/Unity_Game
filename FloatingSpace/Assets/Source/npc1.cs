544cf7c7f4435c7b8dc520abba3420fb8850014f
chloejiwon <chloe326o.o@gmail.com>
2017-11-16 14:49:08 +0900

    NPC1 + Collision Detect

---
+++ FloatingSpace/Assets/Source/npc1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc1 : MonoBehaviour {
+
+	public float rotateSpeed = 1;
+	public GameObject start_dialogue;
+
+
+	// Use this for initialsization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.back * rotateSpeed);
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		// npc 가 player와 충돌 했을 때 
+		// 대화 시작
+		print("collision Enter");
+
+		if (other.gameObject.tag.Equals ("Player")) {
+			// Player?
+			// Dialogue begins
+			// STOP JUST FOR NOW
+
+			print("Collide");
+
+			other.gameObject.GetComponent<PlayerController> ().canMove = false;
+
+		}
+
+	
+	}
+
+}
